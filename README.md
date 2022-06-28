4c97bbbacd59c7fd0ef23de48a1e0723445c5e66
Joaquin <joacofum@gmail.com>
2022-06-28 19:07:19 -0300

    segundo commit, leer readme para ver anotaciones

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - Creación de carpetas para la vista y para los objetos. En vista se creó un index.html que contendrá el canvas (un rectángulo sobre el 
 cual se podrá jugar al ping pong luego) y en la carpeta js se crearon los objetos que serán utilizados para que el juego funcione
 correctamente. De momento se tiene un modelo llamado Board, una vista llamada BoardView y una especie de controlador en la función main donde se instancian los objetos.
+
+## Segundo commit
+- A lo anteriormente creado en el primer commit se le agrega la creación del objeto Barra (Bar). Se crearon los métodos up y down en la barra pero de momento están vacíos,
+su funcionalidad será permitir que la barra se mueva hacia arriba o hacía abajo en el Canvas. Además se definió un método draw en BoardView que permite dibujar un elemento
+en el Canvas dependiendo de qué tipo sea. Al utilizar esta función, de momento se crea la Barra en el Canvas, pero se sigue sin tener el objeto Bola (Ball) creado, es decir,
+es nulo.
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//Modelo
+
+//Tablero
 (function () {
     self.Board = function(width, height){
             this.width = width;
@@ -5,18 +8,43 @@
             this.playing = false;
             this.game_over = false;
             this.bars = [];
-            this.ball = null
+            this.ball = null;
         }
     
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 })();
 
+//Barras
+(function(){
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+
+        //indica de que forma geométrica es la barra para dibujarla en el canvas
+        this.kind = "rectangle";
+    }
+
+    self.Bar.prototype = {
+        down: function(){
+
+        },
+        up: function(){
+
+        }
+    }
+})();
+
+//Vista
 (function(){
     self.BoardView = function(canvas, board){
         this.canvas = canvas;
@@ -25,12 +53,39 @@
         this.board = board;
         this.ctx = canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for(var i = this.board.elements.length -1; i >= 0; i--){
+                var el = this.board.elements[i];
+                //Dibujo el elemento.
+                draw(this.ctx, el);
+            }
+        }
+    }
+
+    //Helpers methods (función fuera de una clase)
+    //Función que dibuja algo en el canvas dependiendo de que tipo sea.
+    function draw(ctx, element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                //Si es cuadrado
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }      
+    }
 })();
 
 self.window.addEventListener("load", main);
 
+//Controlador
 function main(){
     var board = new Board(800, 400);
+    var bar = new Bar(20, 100, 40, 100, board);
+    var bar = new Bar(735, 100, 40, 100, board);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas, board);
+    board_view.draw();
 }
